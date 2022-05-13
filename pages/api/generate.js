4717a56a85455f5fbc9ab32e683ec1375af312ae
ambersweep <applemix5@gmail.com>
2022-05-13 03:21:50 -0400

    UI updates and added prompt examples

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -8,20 +8,8 @@ const openai = new OpenAIApi(configuration);
 export default async function (req, res) {
   const completion = await openai.createCompletion(req.body.engine, {
     prompt: req.body.promptBox,
+    max_tokens: 100,
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
-}
-
-function generatePrompt(animal) {
-  const response =
-    animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is jumpy.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${response}
-Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import { FaRobot } from 'react-icons/fa';
 import Head from "next/head";
 import React, { useState } from "react";
 import {
@@ -35,6 +36,7 @@ export default function Home() {
     });
     const data = await response.json();
     setResult(data.result);
+    console.log(result)
   }
 
   return (
@@ -45,7 +47,8 @@ export default function Home() {
 
       <div>
         <Navbar color="light" expand fixed="">
-          <NavbarBrand href="/">OpenAI Engine Tester</NavbarBrand>
+        <FaRobot size="2em" className="mr-2"/>
+          <NavbarBrand href="/"> OpenAI Engine Tester</NavbarBrand>
           <NavbarToggler onClick={function noRefCheck() {}} />
           <Collapse navbar>
             <Nav className="me-auto" navbar>
@@ -71,9 +74,10 @@ export default function Home() {
               value={prompt}
               onChange={(e) => setPrompt(e.target.value)}
             />
+            
 
             {/* Displays current engine */}
-            <p>Current Engine: {engineText}</p>
+            <p>{!engine ? " Please Select An Engine" : "Current Engine: " + engineText}</p>
 
             {/* Dropdown menu for choosing an engine */}
             <div className="row justify-content-center">
@@ -106,9 +110,54 @@ export default function Home() {
                   >
                     Babbage
                   </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem
+                    onClick={(e) => {
+                      setEngine("text-ada-001");
+                      setEngineText("Ada");
+                    }}
+                  >
+                    Ada
+                  </DropdownItem>
                 </DropdownMenu>
               </UncontrolledButtonDropdown>
-
+              {/* Dropdown for choosing example prompts */}
+              <UncontrolledButtonDropdown>
+                <DropdownToggle color="info" className="ml-2" caret>Prompt Examples</DropdownToggle>
+                <DropdownMenu>
+                  <DropdownItem
+                    onClick={(e) => {
+                      setPrompt("Write a haiku about frogs.");
+                    }}
+                  >
+                    Haiku Maker
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem
+                    onClick={(e) => {
+                      setPrompt("Convert 10 USD into Yen.");
+                    }}
+                  >
+                    Currency Converter
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem
+                    onClick={(e) => {
+                      setPrompt("Tell me how to treat a burn.");
+                    }}
+                  >
+                    First Aid Instructions
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem
+                    onClick={(e) => {
+                      setPrompt("Make me a short lunch recipe.");
+                    }}
+                  >
+                    Recipe Creator
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledButtonDropdown>
               {/*Submits prompt */}
               <Button type="submit" color="primary" className="ml-2">
                 {" "}
@@ -118,13 +167,12 @@ export default function Home() {
           </Form>
 
           <hr></hr>
-
-          <div>Prompt: {prompt}</div>
-          <div>Response:{result}</div>
-
-          <div></div>
+          <h2>Responses </h2>
+          <div><strong>Prompt: </strong>{prompt}</div>
+          <div><p><strong>Response:</strong>{result}</p></div>
         </div>
       </main>
     </div>
   );
+
 }
