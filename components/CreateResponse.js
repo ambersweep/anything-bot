4482092488d693a395f01f8367dd3315b82c845f
ambersweep <applemix5@gmail.com>
2022-05-13 10:30:31 -0400

    formatting and UI improvements

--- components/CreateResponse.js
+++ components/CreateResponse.js
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
-import { Card, CardBody, CardText, CardTitle} from "reactstrap";
+import { Card, CardBody, CardText, CardTitle } from "reactstrap";
 
-export default function CreateResponse({prompt, response}){
-return (
+export default function CreateResponse({ prompt, response }) {
+  return (
     <div>
-  <Card
-  body
-  >
-    <CardBody>
-      <CardTitle tag="h6" className="text-left">
-        <strong>Prompt: </strong> {prompt}
-      </CardTitle>
-      <CardTitle
-        tag="h6"
-        className="text-left"
-      >
-       <strong>Response: </strong> {response}
-      </CardTitle>
-    </CardBody>
-  </Card>
-</div>
-)
+      <Card style={{ width: "25rem" }} body>
+        <CardBody>
+          <CardTitle tag="h6" className="text-left text-wrap">
+            <strong>Prompt: </strong>
+          </CardTitle>
+          <CardText className="text-left">{prompt}</CardText>
+          <CardTitle tag="h6" className="text-left text-wrap">
+            <strong>Response: </strong>
+          </CardTitle>
+          <CardText className="text-left">{response}</CardText>
+        </CardBody>
+      </Card>
+    </div>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { FaRobot, FaGithub, FaLinkedin, FaGlobe } from "react-icons/fa";
+import { FaCheck } from "react-icons/fa";
 import Head from "next/head";
 import React, { useState } from "react";
 import {
@@ -18,7 +18,7 @@ export default function Home() {
   const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState();
   const [engine, setEngine] = useState("");
-  const [engineText, setEngineText] = useState("");
+  const [engineText, setEngineText] = useState();
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -40,134 +40,166 @@ export default function Home() {
         <title>OpenAI Tester</title>
       </Head>
 
-<body className="bg-light" style={{height:"100%"}}>
-      <div className="container-fluid">
-       <Header/>
-      </div>
-      <div className="container border-bottom"></div>
-      <br></br>
-      <main className="container-fluid d-flex justify-content-center align-items-center">
-        <div className="text-center">
-          <Form onSubmit={onSubmit}>
-            <textarea
-              name="promptBox"
-              className="form-control"
-              rows="5"
-              cols="40"
-              placeholder="Enter a prompt..."
-              value={prompt}
-              onChange={(e) => setPrompt(e.target.value)}
-            />
-
-            {/* Displays current engine. If no engine is selected, displays warning text */}
-            <p className="mt-2">
-              {!engine ? (
-                <p className="text-danger mt-2"> Please Select An Engine </p>
-              ) : (
-                "Current Engine: " + engineText
-              )}
-            </p>
-
-            {/* Dropdown menu for choosing an engine */}
-            <div className="justify-content-center">
-              <UncontrolledButtonDropdown>
-                <DropdownToggle caret>Choose Engine</DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem
-                    onClick={(e) => {
-                      setEngine("text-davinci-002");
-                      setEngineText("DaVinci");
-                    }}
-                  >
-                    DaVinci
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    onClick={(e) => {
-                      setEngine("text-curie-001");
-                      setEngineText("Curie");
-                    }}
-                  >
-                    Curie (Recommended)
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    onClick={(e) => {
-                      setEngine("text-babbage-001");
-                      setEngineText("Babbage");
-                    }}
-                  >
-                    Babbage
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    onClick={(e) => {
-                      setEngine("text-ada-001");
-                      setEngineText("Ada");
-                    }}
-                  >
-                    Ada
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledButtonDropdown>
-              {/* Dropdown for choosing example prompts */}
-              <UncontrolledButtonDropdown>
-                <DropdownToggle color="info" className="ml-2" caret>
-                  Prompt Examples
-                </DropdownToggle>
-                <DropdownMenu>
-                  <DropdownItem
-                    onClick={(e) => {
-                      setPrompt("Write a haiku about frogs.");
-                    }}
-                  >
-                    Haiku Maker
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    onClick={(e) => {
-                      setPrompt("Make me a quirky name for my pet rock store.");
-                    }}
-                  >
-                    Shop Namer
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    onClick={(e) => {
-                      setPrompt("Write me a clickbait blog title about skateboarding.");
-                    }}
-                  >
-                    Clickbait Generator
-                  </DropdownItem>
-                  <DropdownItem divider />
-                  <DropdownItem
-                    onClick={(e) => {
-                      setPrompt("Make me a fruity drink recipe and give it a name.");
-                    }}
-                  >
-                    Drink Creator
-                  </DropdownItem>
-                </DropdownMenu>
-              </UncontrolledButtonDropdown>
-
-              {/*Submits prompt */}
-              <Button type="submit" color="primary" className="ml-2">
-                {" "}
-                Submit Prompt{" "}
-              </Button>
-            </div>
-          </Form>
-
-          <hr></hr>
-          <h3>Responses</h3>
-          <br></br>
-        <div className="container">
-            <CreateResponse prompt={prompt} response={result}/>
-          </div>
+      <body className="bg-light" style={{ height: "100%" }}>
+        <Header />
+        <br></br>
+        <div className="container text-center">
+          <p>
+            Welcome to the <mark>OpenAI Engine Tester!</mark> Your playground
+            for experimenting with the OpenAI API.
+          </p>
+          <p>
+            {" "}
+            Write out a prompt, or try one of the example prompts below to
+            experience the magic! 🪄
+          </p>
+          <p>
+            <small>
+              You can learn more about OpenAI{" "}
+              <a href="https://beta.openai.com" target="_blank">
+                here
+              </a>
+              .
+            </small>
+          </p>
         </div>
-      </main>
-      <Footer/>
+        <main className="container-flex d-flex justify-content-center align-items-center">
+          <div className="text-center">
+            <Form onSubmit={onSubmit}>
+              <textarea
+                name="promptBox"
+                cols="40"
+                placeholder="Enter a prompt..."
+                value={prompt}
+                onChange={(e) => setPrompt(e.target.value)}
+                required
+              />
+
+              {/* Displays current engine. If no engine is selected, displays warning text */}
+              <p className="mt-2">
+                {!engine ? (
+                  <p className="text-secondary mt-2">
+                    {" "}
+                    Please select an engine{" "}
+                  </p>
+                ) : (
+                  "Current Engine: " + engineText
+                )}
+              </p>
+
+              {/* Dropdown menu for choosing an engine */}
+              <div className="row justify-content-center">
+                <div className="col-xs-4">
+                  <UncontrolledButtonDropdown>
+                    <DropdownToggle caret className="mt-2">
+                      Choose Engine
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem
+                        onClick={(e) => {
+                          setEngine("text-davinci-002");
+                          setEngineText("DaVinci");
+                        }}
+                      >
+                        DaVinci
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        onClick={(e) => {
+                          setEngine("text-curie-001");
+                          setEngineText("Curie");
+                        }}
+                      >
+                        Curie (Recommended)
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        onClick={(e) => {
+                          setEngine("text-babbage-001");
+                          setEngineText("Babbage");
+                        }}
+                      >
+                        Babbage
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        onClick={(e) => {
+                          setEngine("text-ada-001");
+                          setEngineText("Ada");
+                        }}
+                      >
+                        Ada
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledButtonDropdown>
+
+                  {/* Dropdown for choosing example prompts */}
+
+                  <UncontrolledButtonDropdown>
+                    <DropdownToggle color="info" className="ml-2 mt-2" caret>
+                      Prompt Examples
+                    </DropdownToggle>
+                    <DropdownMenu>
+                      <DropdownItem
+                        onClick={(e) => {
+                          setPrompt("Write a haiku about frogs.");
+                        }}
+                      >
+                        Haiku Maker
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        onClick={(e) => {
+                          setPrompt(
+                            "Make me a quirky name for my pet rock store."
+                          );
+                        }}
+                      >
+                        Shop Namer
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        onClick={(e) => {
+                          setPrompt(
+                            "Write me a clickbait blog title about skateboarding."
+                          );
+                        }}
+                      >
+                        Clickbait Generator
+                      </DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem
+                        onClick={(e) => {
+                          setPrompt(
+                            "Make me a fruity drink recipe and give it a name."
+                          );
+                        }}
+                      >
+                        Drink Creator
+                      </DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledButtonDropdown>
+
+                  {/*Submits prompt */}
+
+                  <Button type="submit" color="primary" className="ml-2 mt-2">
+                    {" "}
+                    <FaCheck /> Submit Prompt{" "}
+                  </Button>
+                </div>
+              </div>
+            </Form>
+
+            <hr></hr>
+            <h4>Responses</h4>
+            <br></br>
+            <div className="container">
+              <CreateResponse prompt={prompt} response={result} />
+            </div>
+          </div>
+        </main>
       </body>
+      <Footer />
     </div>
   );
 }
