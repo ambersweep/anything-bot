fa9ca87bf70d259709c2960875aa242eb7fdfd0c
ambersweep <applemix5@gmail.com>
2022-05-13 01:10:09 -0400

    init commit with basic functionality

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -6,22 +6,22 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 export default async function (req, res) {
-  const completion = await openai.createCompletion("text-davinci-002", {
-    prompt: generatePrompt(req.body.animal),
+  const completion = await openai.createCompletion(req.body.engine, {
+    prompt: req.body.promptBox,
     temperature: 0.6,
   });
   res.status(200).json({ result: completion.data.choices[0].text });
 }
 
 function generatePrompt(animal) {
-  const capitalizedAnimal =
+  const response =
     animal[0].toUpperCase() + animal.slice(1).toLowerCase();
-  return `Suggest three names for an animal that is a superhero.
+  return `Suggest three names for an animal that is jumpy.
 
 Animal: Cat
 Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
 Animal: Dog
 Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: ${capitalizedAnimal}
+Animal: ${response}
 Names:`;
 }
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,12 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Head from "next/head";
-import { useState } from "react";
-import styles from "./index.module.css";
+import React, { useState } from "react";
+import { Button, Form, ButtonGroup } from 'reactstrap';
 
 export default function Home() {
-  const [animalInput, setAnimalInput] = useState("");
+  const [prompt, setPrompt] = useState("");
   const [result, setResult] = useState();
+  const [engine, setEngine] = useState("")
 
   async function onSubmit(event) {
     event.preventDefault();
@@ -13,34 +15,60 @@ export default function Home() {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ animal: animalInput }),
+      body: JSON.stringify({ promptBox: prompt, engine: engine }),
     });
     const data = await response.json();
     setResult(data.result);
-    setAnimalInput("");
   }
 
   return (
     <div>
       <Head>
-        <title>OpenAI Quickstart</title>
-        <link rel="icon" href="/dog.png" />
+        <title>OpenAI Tester</title>
       </Head>
 
-      <main className={styles.main}>
-        <img src="/dog.png" className={styles.icon} />
-        <h3>Name my pet</h3>
-        <form onSubmit={onSubmit}>
-          <input
-            type="text"
-            name="animal"
-            placeholder="Enter an animal"
-            value={animalInput}
-            onChange={(e) => setAnimalInput(e.target.value)}
+      <main className="container d-flex justify-content-center align-items-center">
+      <div className="col-md-3">
+
+        <h3 className="text-center">Fun with AI</h3>
+        <p>using: {engine}</p>
+        <Form onSubmit={onSubmit}>
+          <textarea
+            name="promptBox"
+            rows="5" cols="45"
+            placeholder="Enter a prompt..."
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
           />
-          <input type="submit" value="Generate names" />
-        </form>
-        <div className={styles.result}>{result}</div>
+          <Button type="submit" color="primary"> Submit Prompt </Button>
+        </Form>
+        <hr></hr>
+        <div>Prompt: {prompt}</div>
+        <div>Response:{result}</div>
+        <div>
+        <ButtonGroup>
+    <Button
+      color="primary"
+      onClick={(e) => setEngine("text-davinci-002")}
+    >
+      DaVinci
+    </Button>
+    <Button
+      color="primary"
+      onClick={(e) => setEngine("text-curie-001")}
+    >
+      Curie
+    </Button>
+    <Button
+      color="primary"
+      onClick={(e) => setEngine("text-babbage-001")}
+    >
+      Babbage
+    </Button>
+  </ButtonGroup>
+  
+        </div>
+        </div>
       </main>
     </div>
   );
